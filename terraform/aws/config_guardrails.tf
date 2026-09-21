174918b97b3244d3f4aa9aa94da621a3627d1453
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-21 09:21:40 -0600

    Fix formatting in config_guardrails.tf

--- terraform/aws/config_guardrails.tf
+++ terraform/aws/config_guardrails.tf
@@ -2,7 +2,6 @@ resource "aws_s3_account_public_access_block" "this" {
   count                   = var.enable_apply ? 1 : 0
   block_public_acls       = var.block_public_acls
   ignore_public_acls      = true
-  block_public_policy     = var.block_public_policy   # <-- add this
+  block_public_policy     = var.block_public_policy
   restrict_public_buckets = true
 }
-
