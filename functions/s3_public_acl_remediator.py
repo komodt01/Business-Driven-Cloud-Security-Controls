8382b7b4421c3a72c57eb2bad2a5ad3b4f9d3a70
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-21 09:25:00 -0600

    Refactor S3 ACL remediation logic for clarity

--- functions/s3_public_acl_remediator.py
+++ functions/s3_public_acl_remediator.py
@@ -1,25 +1,44 @@
 import os
+
 import boto3
 
-# Allow LocalStack via AWS_ENDPOINT_URL; harmless on real AWS
-s3 = boto3.client("s3", endpoint_url=os.getenv("AWS_ENDPOINT_URL"))
 
 RISKY_URIS = {
     "http://acs.amazonaws.com/groups/global/AllUsers",
     "http://acs.amazonaws.com/groups/global/AuthenticatedUsers",
 }
 
+s3 = boto3.client(
+    "s3",
+    endpoint_url=os.getenv("AWS_ENDPOINT_URL"),
+)
+
+
 def handler(event, context):
     """
-    Remove public ACL grants from the bucket referenced in the CloudTrail/EventBridge event.
-    Expects: event["detail"]["requestParameters"]["bucketName"]
-    Returns: {"remediated": <bucket>, "grants_removed": <int>}
+    Remove public ACL grants from the S3 bucket identified in the event.
     """
     bucket = event["detail"]["requestParameters"]["bucketName"]
+
     acl = s3.get_bucket_acl(Bucket=bucket)
-    new_grants = [g for g in acl["Grants"] if g.get("Grantee", {}).get("URI") not in RISKY_URIS]
+
+    approved_grants = [
+        grant
+        for grant in acl["Grants"]
+        if grant.get("Grantee", {}).get("URI") not in RISKY_URIS
+    ]
+
+    grants_removed = len(acl["Grants"]) - len(approved_grants)
+
     s3.put_bucket_acl(
         Bucket=bucket,
-        AccessControlPolicy={"Grants": new_grants, "Owner": acl["Owner"]},
+        AccessControlPolicy={
+            "Grants": approved_grants,
+            "Owner": acl["Owner"],
+        },
     )
-    return {"remediated": bucket, "grants_removed": len(acl["Grants"]) - len(new_grants)}
+
+    return {
+        "remediated": bucket,
+        "grants_removed": grants_removed,
+    }
