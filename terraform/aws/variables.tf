4f41a63cf064f0e34c782b01599ebdae487e4b6d
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-21 09:24:15 -0600

    Update variable descriptions in variables.tf

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,30 +1,29 @@
 variable "region" {
   type        = string
   default     = "us-east-1"
-  description = "AWS region for sandbox"
+  description = "AWS region used for the security controls demo."
 }
 
 variable "enable_apply" {
   type        = bool
   default     = false
-  description = "Guard: create resources only when true (sandbox)."
+  description = "Controls whether AWS resources are created for the demo."
 }
 
 variable "lambda_name" {
   type        = string
   default     = "controls-demo-remediator"
-  description = "Name of the corrective Lambda"
+  description = "Name of the S3 ACL remediation Lambda function."
 }
 
 variable "block_public_acls" {
   type        = bool
   default     = true
-  description = "Temporarily set false to allow a test of ACL remediation."
+  description = "Controls whether new public S3 ACLs are blocked at the account level."
 }
+
 variable "block_public_policy" {
   type        = bool
   default     = true
-  description = "Temporarily set false to allow a bucket-policy remediation test."
+  description = "Controls whether new public S3 bucket policies are blocked at the account level."
 }
-
-
