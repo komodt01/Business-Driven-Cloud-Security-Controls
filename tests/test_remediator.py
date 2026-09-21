8d220971b57320d4adea7a55b482518d675926aa
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-21 09:25:31 -0600

    Rename test function and improve assertions

--- tests/test_remediator.py
+++ tests/test_remediator.py
@@ -1,24 +1,41 @@
 import boto3
 from moto import mock_aws
+
 from functions.s3_public_acl_remediator import handler
 
+
+RISKY_URIS = {
+    "http://acs.amazonaws.com/groups/global/AllUsers",
+    "http://acs.amazonaws.com/groups/global/AuthenticatedUsers",
+}
+
+
 @mock_aws
-def test_removes_public_grants():
+def test_removes_public_acl_grants():
     s3 = boto3.client("s3", region_name="us-east-1")
+
     s3.create_bucket(Bucket="demo")
-    # Make the bucket risky
     s3.put_bucket_acl(Bucket="demo", ACL="public-read")
 
-    event = {"detail": {"requestParameters": {"bucketName": "demo"}}}
-    out = handler(event, None)
-    assert out["remediated"] == "demo"
+    event = {
+        "detail": {
+            "requestParameters": {
+                "bucketName": "demo"
+            }
+        }
+    }
+
+    result = handler(event, None)
+
+    assert result["remediated"] == "demo"
+    assert result["grants_removed"] > 0
 
     acl = s3.get_bucket_acl(Bucket="demo")
-    risky = [
-        g for g in acl["Grants"]
-        if g["Grantee"].get("URI") in (
-            "http://acs.amazonaws.com/groups/global/AllUsers",
-            "http://acs.amazonaws.com/groups/global/AuthenticatedUsers",
-        )
+
+    risky_grants = [
+        grant
+        for grant in acl["Grants"]
+        if grant.get("Grantee", {}).get("URI") in RISKY_URIS
     ]
-    assert risky == []
+
+    assert risky_grants == []
